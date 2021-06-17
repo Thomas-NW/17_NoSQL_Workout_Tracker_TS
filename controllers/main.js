20ca19d3f5c7a1c7dbf546a9eb9af948131885e2
Thomas Schmidt <thomasschmidt2021@u.northwestern.edu>
2021-06-17 17:37:30 -0500

    change api index file name

--- controllers/main.js
+++ controllers/main.js
@@ -2,7 +2,7 @@
  const router = require('express').Router();
 //  const apiRoutes = require('./api');
  const homeRoutes = require('./homeroutes');
- const apiRoutes = require('./api');
+ const apiRoutes = require('./api/main.js');
  
  
  router.use('/api', apiRoutes);
